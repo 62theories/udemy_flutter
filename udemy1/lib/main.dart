58db29e6e52fd03ecd7337c7db64884be8b56820
ball <nattawatx86@gmail.com>
2021-06-02 07:45:56 +0700

    แสดง text ธรรมดา

---
+++ udemy1/lib/main.dart
@@ -0,0 +1,13 @@
+// 1 import lib
+import 'package:flutter/material.dart';
+
+// 2 define  main function
+void main() {
+  // 3 create a new text widget
+  var app = MaterialApp(
+    home: Text('Hi there'),
+  );
+
+  // 4 show the text widget on screen
+  runApp(app);
+}
