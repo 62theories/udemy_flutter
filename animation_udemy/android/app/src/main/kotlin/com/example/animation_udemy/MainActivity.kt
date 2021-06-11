0a653a7483f2df63f491e93d16280f4c732cb48c
ball <nattawatx86@gmail.com>
2021-06-11 08:34:16 +0700

    สร้าง project ไว้เรียน animation

---
+++ animation_udemy/android/app/src/main/kotlin/com/example/animation_udemy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animation_udemy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ blocvsprovider/android/app/src/main/kotlin/com/example/blocvsprovider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.blocvsprovider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
